1bbb96f8a3cbc8f6a7433c7f36b151bb20bc5487
Samuel-Tomisin <samueltomisin01@gmail.com>
2026-09-20 17:31:24 +0100

    new commit

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -156,7 +156,7 @@ function RecentTransactionsCard() {
 
 export default function Hero() {
   return (
-    <section className="relative bg-primary pl-2 sm:pl-10 sm:pt-7 lg:pl-16 lg:pt-7 flex flex-col items-center text-center lg:items-start lg:text-left">
+    <section className="relative bg-primary pl-2 sm:pl-10 sm:pt-7 lg:pl-16 lg:pt-7 flex">
       {/* Soft decorative swoosh behind the photo */}
       <svg
         className="pointer-events-none absolute inset-0 hidden h-full w-full lg:block"
